492b42c85b3a828d60712be475bc10a6c30ee496
Nate Harris <nathanharris.sf@gmail.com>
2020-05-09 18:01:37 -0700

    Updates

---
+++ precache-manifest.ba140424674eadb230c7ba0d723c436c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfafea686a88185312dba60619542ca9",
+    "url": "/food-diary/index.html"
+  },
+  {
+    "revision": "6c74af8f79c7e004d827",
+    "url": "/food-diary/static/css/2.5f743be1.chunk.css"
+  },
+  {
+    "revision": "61236b0e3d14351b48b0",
+    "url": "/food-diary/static/css/main.917c832b.chunk.css"
+  },
+  {
+    "revision": "6c74af8f79c7e004d827",
+    "url": "/food-diary/static/js/2.83bf84e6.chunk.js"
+  },
+  {
+    "revision": "2089965cc3e0fbb1dda2d650d8277df4",
+    "url": "/food-diary/static/js/2.83bf84e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61236b0e3d14351b48b0",
+    "url": "/food-diary/static/js/main.a613708b.chunk.js"
+  },
+  {
+    "revision": "4f3a2324aaeb4bdbd7cd",
+    "url": "/food-diary/static/js/runtime-main.e25fbbb8.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/food-diary/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/food-diary/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/food-diary/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/food-diary/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/food-diary/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/food-diary/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/food-diary/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/food-diary/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/food-diary/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/food-diary/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/food-diary/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/food-diary/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/food-diary/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/food-diary/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/food-diary/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/food-diary/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
